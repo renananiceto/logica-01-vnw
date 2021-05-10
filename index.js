f6cc8a5dfd443cee12b3e3a58caf56c5db849461
renananiceto <renan.a.ferreira94@gmail.com>
2021-05-10 18:51:21 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const nome = 'Renan Aniceto';
+let idade = 27;
+let ano = 2021;
+const souhumano = true
+let numero1 = 2
+let numero2 = 3
+console.log('meu ano de nascimento é ', ano - idade)
+console.log(numero1 + numero2)
